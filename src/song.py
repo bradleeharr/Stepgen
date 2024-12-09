13713e24e056dc9f6e2f73bd75211e03f1611bf4
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-12-08 23:11:55 -0600

    Add __init__.py to src/ and tests/

--- src/song.py
+++ src/song.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+from stepfileheader import StepFileHeader
+from stepfilenotes import StepFileNotes
+
 class StepFileType(Enum):
     SM = "StepMania Simfile"
     SSC = "StepMania Spinal Shark Collective Format"
@@ -13,38 +16,8 @@ class Song:
         self.banner_file = None # PNG
         self.step_file = StepFileType.UNKNOWN # .STEP   
 
-
-        self.header = {
-            "TITLE": "",
-            "SUBTITLE" : "",
-            "ARTIST" : "",
-
-        }
-        self.title=""
-        self.subtitle=""
-        self.artist=""
-        self.titletranslit="" 
-        #transliteration, alternative or romanized version of the title for easier readability
-        self.subtitletranslit=""
-        self.artisttranslit=""
-        self.genre=""
-        self.credit=""
-        self.banner=""
-        self.background=""
-        self.lyricspath=""
-        self.cdtitle=""
-        self.music=""
-        self.offset=-1
-        self.samplestart=-1
-        self.selectable=None
-        self.displaybpm=None
-        self.bpms=[]
-        self.stops=[]
-        self.timesignatures=[]
-        self.bgchanges=[]
-        self.keysounds=[]
-
-        self.out_file = "TEST_out_stepfile"
+        self.header = StepFileHeader()
+        self.notes = StepFileNotes()
 
     def to_stepfile(self, out_file):
         with open(out_file, "w") as f:
