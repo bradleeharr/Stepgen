24f94f9c6bc6738d49fbe1511c887471f851c416
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-12-09 23:09:52 -0600

    Update tests to include three songs total and fix bpm tolerance. Bpm tolerance refers to the percentage difference that will be tolerated by the pytest.approx() method (currently 2%). Current bpm estimation seems to be slightly out of this tolerance. May consider using altenrative solutions outside of librosa or calibrating to fix inaccuracies.

---
+++ tests/test_assets/ATTRIBUTION.md
@@ -0,0 +1,15 @@
+### File: Fluffing-a-Duck.mp3
+
+Fluffing a Duck Kevin MacLeod | https://incompetech.com/
+Licensed under Creative Commons: By Attribution 3.0 License
+http://creativecommons.org/licenses/by/3.0/
+Music promoted by https://www.chosic.com/free-music/all/
+
+---
+
+### File: Sneaky-Snitch.mp3
+
+Sneaky Snitch by Kevin MacLeod | https://incompetech.com/
+Music promoted by https://www.chosic.com/free-music/all/
+Creative Commons CC BY 3.0
+https://creativecommons.org/licenses/by/3.0/
--- tests/test_music_to_bpm.py
+++ tests/test_music_to_bpm.py
@@ -3,8 +3,18 @@ import pytest
 from src.music_to_bpm import music_file_to_bpm
 
 def test_BPM():
-    file = "tests/test_assets/Test-Song.wav"
+    dir = "tests/test_assets"
 
-    bpm = music_file_to_bpm(file)
-    print(bpm)
-    assert bpm == pytest.approx(120, 1)
+    files = ["Test-Song1.wav", "Fluffing-a-Duck.mp3", "Sneaky-Snitch.mp3"]
+
+    est_bpms = []
+    for file in files:
+        est_bpms.append(music_file_to_bpm(f"{dir}/{file}"))
+
+    expected_bpms = [120, 123, 87]
+    bpm_tolerance = 0.02
+    
+    for est_bpm, expected_bpm in zip(est_bpms, expected_bpms):
+        print(f"Estimated: {est_bpm} | Actual {expected_bpm}")
+        assert est_bpm == pytest.approx(expected_bpm, bpm_tolerance)
+
