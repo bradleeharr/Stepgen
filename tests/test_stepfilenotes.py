9e4cd95fc0a47314ef72adbee93098a7389b8b8d
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-12-16 17:42:44 -0600

    test_Note_to_file in test_stepfilenotes will now check that beatsList is generated correctly. TODO check if file is tested correctly

--- tests/test_stepfilenotes.py
+++ tests/test_stepfilenotes.py
@@ -7,8 +7,20 @@ def test_Notes_to_file():
     assert uut.difficulty == Difficulty.EASY
     assert uut.difficulty_level == 0
     
-    uut.add_note(NoteType.QUARTER)
+    uut.add_note(NoteType.QUARTER, [Direction.DOWN])
+    uut.add_rest(NoteType.QUARTER)
+    uut.add_note(NoteType.QUARTER, [Direction.LEFT])
+
+    [print("\n",row) for row in uut.beatsList]
+    assert uut.beatsList == [
+        [[0, 0, 1, 0], [0, 0, 1, 0]],
+        [[0, 0, 0, 0], [0, 0, 0, 0,]],
+        [[1, 0, 0, 0], [1, 0, 0, 0,]]
+    ]
 
 def test_Parse_Wav_File():
-    a = StepFile()
+    pass
+    #a = StepFile()
     
+if __name__ == '__main__':
+    test_Notes_to_file()
