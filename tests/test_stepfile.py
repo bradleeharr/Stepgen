f038fcfc4d003b8d830c4d1380d7f1e19ef51375
Bradlee Harrison <bradlee.harrison22@gmail.com>
2024-12-17 22:03:57 -0600

    Add notes to test_stepfile to test creating a stepfile with some notes

--- tests/test_stepfile.py
+++ tests/test_stepfile.py
@@ -1,13 +1,19 @@
 from src.stepfile import StepFile
 
+from src.stepfileenums import NoteType, Direction
+
+
+
+
 def test_Song_to_file():
     a = StepFile()
     a.header.title = "Test Step File"
     a.header.artist = "TestArtist"
     
-    a.notes.beatsList = [
-        
-    ]
+    a.notes.add_note(NoteType.QUARTER, [Direction.DOWN, Direction.UP])
+    a.notes.add_note(NoteType.QUARTER, [Direction.UP])
+    a.notes.add_note(NoteType.QUARTER, [Direction.DOWN])
+    a.notes.add_note(NoteType.QUARTER, [Direction.LEFT, Direction.RIGHT])
 
     a.to_stepfile("test_stepfile.sm")
 
