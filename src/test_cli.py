733d61b0af002ecb23a7faa2af495a3e462dd4ce
Bradlee Harrison <bradlee@git.com>
2026-05-24 14:41:22 -0500

    Update test CLI to allow for launching UI

--- src/test_cli.py
+++ src/test_cli.py
@@ -1,4 +1,12 @@
+import src.ui.main as ui
 
 def main():
 	print("Test Output Success")
 
+	a = input("Would you like to launch the UI? [Yy/Nn]: \n").strip()
+
+	if a == 'y' or a == 'Y':
+		ui.main()
+
+	print("Thank you for using stepgen!")
+	print(r"  If you had any issues, please make an issue on the GitHub!")
